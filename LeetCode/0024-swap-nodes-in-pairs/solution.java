06b17585767aa6b10d8047b9e6686cc7e2c397e9
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-01-29 08:03:43 +0000

    [LeetCode Sync] Solved Runtime - 0 ms (100.00%), Memory - 43.3 MB (12.16%)

---
+++ LeetCode/0024-swap-nodes-in-pairs/solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode temp = dummy;
+        ListNode first ;
+        ListNode second ;
+        while(temp.next!=null && temp.next.next!=null){
+            first = temp.next;
+            second = first.next;
+            temp.next = second;
+            first.next=second.next;
+            second.next = first;
+            temp = first;
+        }
+        return dummy.next;
+    }
+}
