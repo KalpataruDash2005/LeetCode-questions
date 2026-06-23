ae5a24d9845746d4929ea456811e16400aff6725
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-06-23 08:17:37 +0000

    [LeetCode Sync] Solved Runtime - 10 ms (26.55%), Memory - 42 MB (52.53%)

--- LeetCode/1013-fibonacci-number/solution.java
+++ LeetCode/1013-fibonacci-number/solution.java
@@ -1,12 +1,6 @@
 class Solution {
     public int fib(int n) {
-        int a=0 , b=1 , temp=0 ;
-       if(n==0) return 0;
-       for(int i = 1 ; i<=n ;i++){
-        temp = b;
-         b= a+b;
-         a = temp;
-       }
-        return a;
+        if(n == 0 || n==1 ) return n;
+        return fib(n-1) + fib(n-2);
     }
 }
