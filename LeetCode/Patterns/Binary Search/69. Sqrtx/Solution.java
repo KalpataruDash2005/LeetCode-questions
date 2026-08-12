20db9325e08e15574d68f286d779ac17fbb6289b
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-08-12 18:12:56 +0530

    Add Java Solution to Patterns/Binary Search

--- LeetCode/Patterns/Binary Search/69. Sqrtx/Solution.java
+++ LeetCode/Patterns/Binary Search/69. Sqrtx/Solution.java
@@ -1,5 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        int low = 1;
+        int high = x;
+        if(x <= 0){
+            return 0;
+        }
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(mid > low){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return low;
     }
 }
