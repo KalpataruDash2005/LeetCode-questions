d3875777c9852620aa4a27f1f68a3ea115f02257
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-08-12 18:11:24 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/69. Sqrtx/Solution.java
+++ LeetCode/Java/Easy/69. Sqrtx/Solution.java
@@ -1,5 +1,15 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        int low = 1;
+        int high = x;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(mid > low){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return low;
     }
 }
