d127245b4f1a3a320b801e489022efa987081bd1
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-08-13 21:12:32 +0530

    Add Easy Java Solution from LeetCode

---
+++ LeetCode/Java/Easy/1539. Kth Missing Positive Number/Solution.java
@@ -0,0 +1,26 @@
+class Solution { 
+    public int findKthPositive(int[] arr, int k) { 
+        int[] arr1 = new int[arr.length + (k + 1)]; 
+        
+        for(int i = 0; i < arr1.length; i++){ 
+            arr1[i] = i + 1; 
+        } 
+        
+        int i = 0, count = 0; 
+        int j = 0; 
+        
+        while(j < arr1.length){ 
+            if(i < arr.length && arr[i] == arr1[j]){ 
+                i++; 
+                j++; 
+            } else { 
+                count++; 
+                if(count == k){ 
+                    return arr1[j]; 
+                } 
+                j++; 
+            } 
+        } 
+        return 0; 
+    } 
+}
