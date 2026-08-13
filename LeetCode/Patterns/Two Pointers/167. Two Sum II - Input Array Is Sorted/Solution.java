599c1de02f82f1e4751b3dd5e133661c5aec67be
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-08-13 20:49:53 +0530

    Add Java Solution to Patterns/Two Pointers

---
+++ LeetCode/Patterns/Two Pointers/167. Two Sum II - Input Array Is Sorted/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+      Map <Integer , Integer> map = new HashMap<>();
+      int result[] = {-1,-1};
+      for(int i = 0 ; i<numbers.length;i++){
+        int x = target-numbers[i];
+        if(!map.containsKey(x)){
+            map.put(numbers[i],i);
+        }
+        else{
+            int position1 = map.get(x)+1;
+            int position2 = i+1;
+            result[0] = position1;
+            result[1] = position2;
+            return result;
+        }
+      }
+      return result;
+    }
+}
