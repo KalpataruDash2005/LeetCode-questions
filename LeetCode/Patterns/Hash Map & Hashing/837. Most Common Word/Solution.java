1d36fc37851b2fb10096cfa8b0cae003a5ffd3cb
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-08-20 08:39:05 +0530

    Add Java Solution to Patterns/Hash Map & Hashing

---
+++ LeetCode/Patterns/Hash Map & Hashing/837. Most Common Word/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        String cleanParagraph = paragraph.toLowerCase().replaceAll("[!?',;.]", " ");
+        String[] arr = cleanParagraph.trim().split("\\s+");
+        
+        List<String> bannedList = Arrays.asList(banned);
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (String val : arr) {
+            if (!val.isEmpty() && !bannedList.contains(val)) {
+                map.put(val, map.getOrDefault(val, 0) + 1);
+            }
+        }
+        
+        String maxWord = "";
+        int maxCount = 0;
+        
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                maxWord = entry.getKey();
+            }
+        }
+        
+        return maxWord;
+    }
+}
