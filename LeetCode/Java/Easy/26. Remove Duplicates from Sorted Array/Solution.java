7551fd76df4d2f6a979f0a98f6dfd53df7fd86a6
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-08-20 08:11:39 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/26. Remove Duplicates from Sorted Array/Solution.java
+++ LeetCode/Java/Easy/26. Remove Duplicates from Sorted Array/Solution.java
@@ -1,16 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        Set<Integer> set = new LinkedHashSet<>();
-        for(int i=0;i<nums.length;i++){
-            int val = nums[i];
-            set.add(val);
-        }
-        int i =0;
-        for(int val : set){
-            nums[i++]=val;
+        HashSet<Integer> set = new HashSet<>();
+        for(int v : nums){
+            set.add(v);
         }
 
-        int k = set.size();
-        return k;
+        int i = 0;
+        for (int v : set) {
+            nums[i++] = v;
+        }
+        return set.size();
     }
 }
