2538095e1c25c71f0409d7290279adc6bcec0d4b
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-08-21 09:27:29 +0530

    Add Java Solution to Patterns/Math

---
+++ LeetCode/Patterns/Math/2469. Convert the Temperature/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double kelvin = celsius + 273.15;
+        double farenheit = celsius *1.80 + 32.00;
+
+        double[] result = new double[2];
+        result[0] = kelvin;
+        result[1] = farenheit;
+
+        return result;
+    }
+}
