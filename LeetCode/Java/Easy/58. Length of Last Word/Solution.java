9789a6088b799cfc88f48283f8f4db7e57f28411
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-08-21 21:54:18 +0530

    Add Easy Java Solution from LeetCode

---
+++ LeetCode/Java/Easy/58. Length of Last Word/Solution.java
@@ -0,0 +1,8 @@
+class Solution { 
+    public int lengthOfLastWord(String s) { 
+        String[] words = s.trim().split("\\s+"); 
+        int lastWordIndex = words.length - 1;
+        String lastWord = words[lastWordIndex]; 
+        return lastWord.length(); 
+    } 
+}
