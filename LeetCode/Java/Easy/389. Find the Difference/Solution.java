3cadc17af3db307a3ac430e7b68fc82f508bf927
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-08-22 22:00:42 +0530

    Add Easy Java Solution from LeetCode

---
+++ LeetCode/Java/Easy/389. Find the Difference/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char res = ' ';
+        for (int i = 0; i < t.length(); i++) {
+            boolean flag = false;
+            for (int j = 0; j < s.length(); j++) {
+                if (t.charAt(i) == s.charAt(j)) {
+                    flag = true;
+                }
+            }
+            if (!flag) {
+                res = t.charAt(i);
+            }
+        }
+        return res;
+    }
+}
