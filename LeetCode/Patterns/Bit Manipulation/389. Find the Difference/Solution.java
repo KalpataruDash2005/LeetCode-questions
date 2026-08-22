469379610f58e80478cda0c3305e248f0b5331fe
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-08-22 22:03:05 +0530

    Add Java Solution to Patterns/Bit Manipulation

--- LeetCode/Patterns/Bit Manipulation/389. Find the Difference/Solution.java
+++ LeetCode/Patterns/Bit Manipulation/389. Find the Difference/Solution.java
@@ -1,20 +1,14 @@
 class Solution {
+
     public char findTheDifference(String s, String t) {
-        char res = ' ';
-        for (int i = 0; i < t.length(); i++) {
-            boolean flag = false;
-            for (int j = 0; j < s.length(); j++) {
-                if (t.charAt(i) == s.charAt(j)) {
-                    flag = true;
-                    s = s.replaceFirst(String.valueOf(s.charAt(j)), "");
-                    break;
-                }
-            }
-            if (!flag) {
-                res = t.charAt(i); 
-                break;
-            }
-        }
-        return res;
-    }
+        
+        int num = 0;
+
+        for(int cs : s.toCharArray()) num -= cs;
+
+        for(int ct : t.toCharArray()) num += ct;
+
+        return (char) (num);
+        
+         }
 }
