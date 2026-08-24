67b7eda15c633108db2faa7216cc9a0b25d41e45
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-08-24 10:18:26 +0530

    Add Java Solution to Patterns/Arrays

---
+++ LeetCode/Patterns/Arrays/1950. Sign of the Product of an Array/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int pro = 1;
+        for(int i = 0 ; i < nums.length ; i++){
+            pro = pro * nums[i];
+        }
+        if(pro == 0){
+            return 0;
+        }else if(pro > 0){
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
