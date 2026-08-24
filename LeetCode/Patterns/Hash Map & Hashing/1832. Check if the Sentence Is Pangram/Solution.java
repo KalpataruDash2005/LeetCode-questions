bfef87a3e6d3c3af1187a42255c826199e94c271
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-08-24 11:24:42 +0530

    Add Java Solution to Patterns/Hash Map & Hashing

---
+++ LeetCode/Patterns/Hash Map & Hashing/1832. Check if the Sentence Is Pangram/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        int []freq = new int[26];
+        for(char ch : sentence.toCharArray()){
+            freq[ch - 'a']++;
+        }
+
+        for(int i = 0 ; i < freq.length ; i++){
+            if(freq[i] < 1){
+                return false;
+            }
+        }
+        return true; 
+    }
+}
