f7b2a945ce4d5222ded987c0e213f31786d28fe1
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-08-24 11:15:46 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/657. Robot Return to Origin/Solution.java
+++ LeetCode/Java/Easy/657. Robot Return to Origin/Solution.java
@@ -4,20 +4,21 @@ class Solution {
        int down = -1;
        int left = 1;
        int right = -1;
-       int moved = 0;
+       int movelr = 0;
+       int movetb = 0;
        for(int i = 0 ; i<moves.length() ; i++){
           if(moves.charAt(i) == 'L'){
-            moved += left;
+            movelr += left;
           }else if(moves.charAt(i) == 'R'){
-            moved += right;
+            movelr += right;
           }else if(moves.charAt(i) == 'U'){
-            moved += up;
+            movetb += up;
           }else{
-            moved += down;
+            movetb += down;
           }
        }
 
-       if(moved == 0 ) return true;
+       if(movelr == 0 && movetb == 0) return true;
        return false;
     }
 }
