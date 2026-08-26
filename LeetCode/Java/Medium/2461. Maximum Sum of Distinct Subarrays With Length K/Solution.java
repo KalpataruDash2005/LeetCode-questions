19d2b1272d8228fa58c4c6787fbe25a37a1bc083
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-08-26 13:19:36 +0530

    Add Medium Java Solution for 2461. Maximum Sum of Distinct Subarrays With Length K from LeetCode

--- LeetCode/Java/Medium/2461. Maximum Sum of Distinct Subarrays With Length K/Solution.java
+++ LeetCode/Java/Medium/2461. Maximum Sum of Distinct Subarrays With Length K/Solution.java
@@ -1,19 +1,44 @@
-class Solution { 
-    public long maximumSubarraySum(int[] nums, int k) { 
-        long ans = 0; 
+class Solution {
+    public long maximumSubarraySum(int[] nums, int k) {
         long sum = 0;
+        long ans = 0;
 
-        for (int i = 0; i <= nums.length - k; i++) {
+        int[] freq = new int[100001];
 
-            if (nums[i] != nums[i + 1] &&
-                nums[i] != nums[i + 2] &&
-                nums[i + 1] != nums[i + 2]) {
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+            freq[nums[i]]++;
+        }
 
-                sum = (long) nums[i] + nums[i + 1] + nums[i + 2];
+        if (getDistinct(freq, nums, 0, k) == k) {
+            ans = sum;
+        }
+
+        for (int i = k; i < nums.length; i++) {
+
+            sum += nums[i];
+            sum -= nums[i - k];
+
+            freq[nums[i]]++;
+            freq[nums[i - k]]--;
+
+            if (getDistinct(freq, nums, i - k + 1, i + 1) == k) {
                 ans = Math.max(ans, sum);
             }
         }
 
         return ans;
-    } 
+    }
+
+    private int getDistinct(int[] freq, int[] nums, int left, int right) {
+        int count = 0;
+
+        for (int i = left; i < right; i++) {
+            if (freq[nums[i]] == 1) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
