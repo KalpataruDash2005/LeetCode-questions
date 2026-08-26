f462d64064436706bf1f0f03a9ab2a6f39bfeb5b
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-08-26 13:10:43 +0530

    Add Medium Java Solution for 2461. Maximum Sum of Distinct Subarrays With Length K from LeetCode

--- LeetCode/Java/Medium/2461. Maximum Sum of Distinct Subarrays With Length K/Solution.java
+++ LeetCode/Java/Medium/2461. Maximum Sum of Distinct Subarrays With Length K/Solution.java
@@ -1,14 +1,19 @@
 class Solution { 
     public long maximumSubarraySum(int[] nums, int k) { 
         long ans = 0; 
-        long sum = 0; 
-        for(int i = 0 ; i<nums.length-2;i++){
-            if(nums[i] != nums[i+1] && nums[i+1] != nums[i+2] && nums[i] != nums[i+2]){
-                sum = nums[i] + nums[i+1] + nums[i+2];
-                ans = sum;
+        long sum = 0;
+
+        for (int i = 0; i <= nums.length - k; i++) {
+
+            if (nums[i] != nums[i + 1] &&
+                nums[i] != nums[i + 2] &&
+                nums[i + 1] != nums[i + 2]) {
+
+                sum = (long) nums[i] + nums[i + 1] + nums[i + 2];
+                ans = Math.max(ans, sum);
             }
         }
-        ans = Math.max(ans,sum);
+
         return ans;
     } 
 }
