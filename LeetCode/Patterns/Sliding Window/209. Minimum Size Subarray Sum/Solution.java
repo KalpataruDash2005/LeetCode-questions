c66d3ba67cb74afde3e80c4d6f807b3080642138
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-08-27 11:09:50 +0530

    Add Medium Java Solution for 209. Minimum Size Subarray Sum from LeetCode

---
+++ LeetCode/Patterns/Sliding Window/209. Minimum Size Subarray Sum/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+       int low = 0;
+       int ans = Integer.MAX_VALUE;
+       int sum = 0;
+       for(int high = 0; high<nums.length ; high++){
+         sum += nums[high];
+        while( sum >= target){
+          ans = Math.min(ans , high - low+1);
+          sum -= nums[low];
+          low++;
+        }
+       
+       }
+       return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+}
