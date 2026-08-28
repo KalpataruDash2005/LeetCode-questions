6f0b4e4944807f35836409039324bb99d517b62b
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-08-28 20:30:26 +0530

    Add Medium Java Solution for 1781. Sum of Beauty of All Substrings from LeetCode

---
+++ LeetCode/Java/Medium/1781. Sum of Beauty of All Substrings/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    public int beautySum(String s) {
+
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int[] freq = new int[26];
+            int max = 0;
+
+            for (int j = i; j < s.length(); j++) {
+
+                freq[s.charAt(j) - 'a']++;
+
+                max = Math.max(max, freq[s.charAt(j) - 'a']);
+
+                int min = Integer.MAX_VALUE;
+
+                for (int k = 0; k < 26; k++) {
+                    if (freq[k] > 0) {
+                        min = Math.min(min, freq[k]);
+                    }
+                }
+
+                sum += max - min;
+            }
+        }
+
+        return sum;
+    }
+}
