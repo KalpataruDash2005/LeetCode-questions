992f178eb713cce165d6ba7eabef9b8afaece873
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-08-29 20:37:45 +0530

    Add Easy Java Solution for 2058. Concatenation of Array from LeetCode

---
+++ LeetCode/Java/Easy/2058. Concatenation of Array/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int arr[] = new int[2*n];
+        int index = 0;
+        for(int i = 0 ; i<nums.length ; i++){
+            arr[index++] = nums[i];
+        }
+
+        for(int i = 0 ; i<nums.length ; i++){
+            arr[index++] = nums[i];
+        }
+        return arr;
+    }
+}
