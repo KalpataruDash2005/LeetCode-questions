d8e03eddfb363bce2daf0045ee0f5c9a36bff7c0
KalpataruDash2005 <kalpataru05aug@gmail.com>
2026-09-07 13:09:41 +0530

    Add Easy Java Solution for 724. Find Pivot Index from LeetCode

--- LeetCode/Patterns/Prefix Sum/724. Find Pivot Index/Solution.java
+++ LeetCode/Patterns/Prefix Sum/724. Find Pivot Index/Solution.java
@@ -1,22 +1,22 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int left = 0;
-        int right = nums.length-1;
-        int lsum = 0;
-        int rsum = 0;
-        while(left < right){
-            if(rsum <= lsum){
-                rsum+=nums[right];
-                right--;
-            }
-            else {
-                lsum += nums[left];
-                left++;
-            }
+        int totalSum = 0;
+        int leftSum = 0;
+        
+        // 1. Calculate the overall sum of the array
+        for (int num : nums) {
+            totalSum += num;
         }
-        if(lsum == rsum){
-            return left;
+        
+        // 2. Scan from left to right to locate the pivot
+        for (int i = 0; i < nums.length; i++) {
+            // Right sum = total sum minus left sum minus the current element
+            if (leftSum == totalSum - leftSum - nums[i]) {
+                return i; // Returns the leftmost valid index immediately
+            }
+            leftSum += nums[i];
         }
+        
         return -1;
     }
 }
